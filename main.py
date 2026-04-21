92da67efed0b8a76a17cba5841bb923471094808
rp <rp>
2026-04-21 17:22:42 +0200

    a

--- main.py
+++ main.py
@@ -34,22 +34,31 @@ def print_serial_messages():
     print(r._wait_for_message())
 
 def main():
-  """
-  thread1=Thread(target=print_serial_messages)
-  thread1.start()
-  thread2=Thread(target=show_video)
-  thread2.start()
-  """
-  while(True):
-    key=keyboard.read_key()
-    dir_number=["down","up","right","left"].index(key)
-    pose=[0,0,0,0,0]
-    pose[math.floor(dir_number/2)]=[1,-1][dir_number%2]*10
-    print(pose)
-    r.jog(pose)
+  #thread1=Thread(target=print_serial_messages)
+  #thread1.start()
+  #thread2=Thread(target=show_video)
+  #thread2.start()
+  default_speed=50
+  r.reset_and_home_joints()
+  r.set_joint_speed(default_speed)
+  r.go_to_pose([400,0,150,180,0])
+  r.set_joint_speed(20)
 
-  #r.go_to_foetus_pos()
-  #r.disable_motors()
+  key=""
+  while(key!="esc"):
+    key=keyboard.read_key()
+    #print(key)
+    accepted_keys=["down","up","right","left"]
+    if key in accepted_keys:
+      dir_number=["down","up","right","left"].index(key)
+      pose=[0,0,0,0,0]
+      pose[math.floor(dir_number/2)]=[1,-1][dir_number%2]*5
+      #print(pose)
+      r.jog(pose)
+
+  r.set_joint_speed(default_speed)
+  r.go_to_foetus_pos()
+  r.disable_motors()
 
 if __name__ == "__main__":
   main()
