0a0739beccc7c69381dd436d08e4178615d9c625
tower <you@example.com>
2026-04-21 17:03:51 +0200

    a

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ INSTALL:
 cd ~/git/teleoperate && python3 -m venv venv && source venv/bin/activate
 pip install pyserial
 pip install opencv-python
+pip install keyboard
 
 LAUNCH:
-cd ~/git/teleoperate && source venv/bin/activate && python main.py
+sudo ~/git/teleoperate/venv/bin/python main.py
 ssh -X galatae@192.168.0.110
--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import numpy as np
 import cv2 as cv
 from threading import Thread
 from robot import Robot
+import keyboard
+import math
 
 r=Robot(False)
-
 
 def show_video():
   cap = cv.VideoCapture(0)
@@ -15,21 +16,16 @@ def show_video():
       print("Cannot open camera")
       exit()
   while True:
-      # Capture frame-by-frame
       ret, frame = cap.read()
   
-      # if frame is read correctly ret is True
       if not ret:
           print("Can't receive frame (stream end?). Exiting ...")
           break
-      # Our operations on the frame come here
       gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-      # Display the resulting frame
       cv.imshow('frame', gray)
       if cv.waitKey(1) == ord('q'):
           break
-  
-  # When everything done, release the capture
+
   cap.release()
   cv.destroyAllWindows()
 
@@ -38,21 +34,22 @@ def print_serial_messages():
     print(r._wait_for_message())
 
 def main():
+  """
   thread1=Thread(target=print_serial_messages)
   thread1.start()
   thread2=Thread(target=show_video)
   thread2.start()
-  
-  try:
-    while(True):
-      message=input()
-      r.send_message(message)
-  except:
-    print(traceback.format_exc())
-    
-  r.go_to_foetus_pos()
-  r.disable_motors()
+  """
+  while(True):
+    key=keyboard.read_key()
+    dir_number=["down","up","right","left"].index(key)
+    pose=[0,0,0,0,0]
+    pose[math.floor(dir_number/2)]=[1,-1][dir_number%2]*10
+    print(pose)
+    r.jog(pose)
 
+  #r.go_to_foetus_pos()
+  #r.disable_motors()
 
 if __name__ == "__main__":
   main()
