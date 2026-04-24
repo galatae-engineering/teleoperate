0c54d2748fcd309c30805a08dd659ef094a4bab4
rp <rp>
2026-04-24 15:49:26 +0200

    first version working kind of ok

--- main.py
+++ main.py
@@ -5,42 +5,40 @@ import numpy as np
 import cv2 as cv
 from threading import Thread
 from robot import Robot
-import keyboard
+#import keyboard
 import math
 import time
 import traceback
+import curses
 
-keys_state=[False,False,False,False,False]
+screen = curses.initscr()
+keyboard_state=[0,0,0,0,0]
 
 def show_video():
   cap = cv.VideoCapture(0)
   if not cap.isOpened():
-      print("Cannot open camera")
-      exit()
+    print("Cannot open camera")
+    exit()
   while True:
-      ret, frame = cap.read()
-  
-      if not ret:
-          print("Can't receive frame (stream end?). Exiting ...")
-          break
-      #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-      cv.imshow('frame', frame)
-      if cv.waitKey(1) == ord('q'):
-          break
+    ret, frame = cap.read()
+
+    if not ret:
+      print("Can't receive frame (stream end?). Exiting ...")
+      break
+    #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) == ord('q'):
+      break
 
   cap.release()
   cv.destroyAllWindows()
 
 def update_keys_state():
-  key_names=["left","right","up","down","esc"]
-  
+  keyboard_numbers=[260,261,259,258,27]
   while(True):
-    event = keyboard.read_event()
-    if event.name in key_names:
-      key_index=key_names.index(event.name)
-      keys_state[key_index]=event.event_type == keyboard.KEY_DOWN
-    #time.sleep(0.01)
-    #print(keys_state)
+    char = screen.getch()
+    if char in keyboard_numbers:
+        keyboard_state[keyboard_numbers.index(char)]=1
 
 def get_direction_from_keys(key_pos,key_neg):
   dir=0
@@ -53,28 +51,42 @@ def get_direction_from_keys(key_pos,key_neg):
 def main():
   video_thread=Thread(target=show_video)
   video_thread.start()
+  
+  curses.noecho()
+  curses.cbreak()
+  screen.keypad(True)
+
   keys_thread=Thread(target=update_keys_state)
   default_speed=50
-  r=Robot(False)
+  
   pose_key_indices=[[3,2],[1,0]]
 
   keys_thread.start()
+  
+  r=Robot(False)
   r.reset_and_home_joints()
   r.set_joint_speed(default_speed)
   r.go_to_pose([400,0,150,180,0])
-  r.set_joint_speed(20)
-  
+  #r.set_joint_speed(50)
+
+  current_state=keyboard_state.copy()
+
   try:
-    while(not keys_state[4]):
+    while(not current_state[4]):
       pose=[0,0,0,0,0]
       for i in range(2):
-        pose[i]=10*get_direction_from_keys(keys_state[pose_key_indices[i][0]],keys_state[pose_key_indices[i][1]])
+        pose[i]=5*get_direction_from_keys(current_state[pose_key_indices[i][0]],current_state[pose_key_indices[i][1]])
+      
       if pose != [0,0,0,0,0]:
         #print(pose)
         r.jog(pose)
-      time.sleep(0.01)
+        for i in range(len(keyboard_state)):
+          keyboard_state[i]=0
+
+      current_state=keyboard_state.copy()
   except:
     print(traceback.format_exc())
+
 
   r.set_joint_speed(default_speed)
   r.go_to_foetus_pos()
