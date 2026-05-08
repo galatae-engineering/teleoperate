85b15c15e021b712f9d6544e7637f5588d30abea
rp <rp>
2026-05-08 18:48:19 +0200

    kind of working but not wel from remote

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-INSTALL:
-
 cd ~/git/teleoperate && python3 -m venv venv && source venv/bin/activate
-pip install pyserial
+python -m pip install "kivy[base]" kivy_examples
 pip install opencv-python
-pip install keyboard
 
-LAUNCH:
-cd ~/git/teleoperate && source venv/bin/activate && sudo venv/bin/python main.py
-ssh -X galatae@192.168.0.110
+cd ~/git/teleoperate && source venv/bin/activate && python main.py
--- main.py
+++ main.py
@@ -1,97 +1,87 @@
 import sys
 sys.path.append('../galatae-api/')
-
-import numpy as np
-import cv2 as cv
-from threading import Thread
 from robot import Robot
-#import keyboard
-import math
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, StringProperty, ListProperty
+from kivy.clock import Clock
+from threading import Thread
 import time
-import traceback
-import curses
 
-screen = curses.initscr()
-keyboard_state=[0,0,0,0,0]
+buttons_state=[False,False,False,False,False,False,False,False,False,False]
+window_is_open=True
 
-def show_video():
-  cap = cv.VideoCapture(0)
-  if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-  while True:
-    ret, frame = cap.read()
+class DirButton(Button):
+  def change_buttons_state(self,is_button_pressed):
+    global buttons_state
+    index=["forward","backward","left","right","up","down","pitch +","pitch -","roll +","roll -"].index(self.text)
+    buttons_state[index]=is_button_pressed
+    print(is_button_pressed)
 
-    if not ret:
-      print("Can't receive frame (stream end?). Exiting ...")
-      break
-    #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame',frame)
-    if cv.waitKey(1) == ord('q'):
-      break
+  def on_press(self):
+    self.change_buttons_state(True)
+    
+  def on_release(self):
+    self.change_buttons_state(False)
 
-  cap.release()
-  cv.destroyAllWindows()
+class FourButtons(GridLayout):
+  names = ListProperty(["","","",""])
+  pass
 
-def update_keys_state():
-  keyboard_numbers=[260,261,259,258,27]
-  while(True):
-    char = screen.getch()
-    if char in keyboard_numbers:
-        keyboard_state[keyboard_numbers.index(char)]=1
+class TwoButtons(GridLayout):
+  names = ListProperty(["",""])
+  pass
 
-def get_direction_from_keys(key_pos,key_neg):
-  dir=0
-  if(key_pos and not key_neg):
-    dir=1
-  elif(key_neg and not key_pos):
-    dir=-1
+class Teleoperate(GridLayout):
+  pass
+
+def get_direction_from_buttons(button_pos,button_neg):
+  dir=button_pos != button_neg
+  
+  if dir:
+    dir=button_pos*2-1
+
   return dir
 
-def main():
-  video_thread=Thread(target=show_video)
-  video_thread.start()
-  
-  curses.noecho()
-  curses.cbreak()
-  screen.keypad(True)
+def move_robot_if_necessary(r):
+  pose=[0,0,0,0,0]
+  pose_buttons_indices=[[1,0],[3,2],[4,5],[6,7],[8,9]]
 
-  keys_thread=Thread(target=update_keys_state)
-  default_speed=50
-  
-  pose_key_indices=[[3,2],[1,0]]
+  for i in range(len(pose)):
+    pose[i]=1*get_direction_from_buttons(buttons_state[pose_buttons_indices[i][0]],buttons_state[pose_buttons_indices[i][1]])
+  if pose != [0,0,0,0,0]:
+    r.jog(pose)
 
-  keys_thread.start()
-  
-  r=Robot(False)
+def control_robot():
+  global window_is_open
+  r=Robot()
   r.reset_and_home_joints()
-  r.set_joint_speed(default_speed)
+  r.set_joint_speed(50)
   r.go_to_pose([400,0,150,180,0])
-  #r.set_joint_speed(50)
 
-  current_state=keyboard_state.copy()
-
-  try:
-    while(not current_state[4]):
-      pose=[0,0,0,0,0]
-      for i in range(2):
-        pose[i]=5*get_direction_from_keys(current_state[pose_key_indices[i][0]],current_state[pose_key_indices[i][1]])
-      
-      if pose != [0,0,0,0,0]:
-        #print(pose)
-        r.jog(pose)
-        for i in range(len(keyboard_state)):
-          keyboard_state[i]=0
-
-      current_state=keyboard_state.copy()
-  except:
-    print(traceback.format_exc())
-
-
-  r.set_joint_speed(default_speed)
+  while(window_is_open):
+    move_robot_if_necessary(r)
+    time.sleep(0.001)
+  
   r.go_to_foetus_pos()
   r.disable_motors()
 
+class MainApp(App):
+  def build(self):
+    return Teleoperate()
+
+def main():
+  global window_is_open
+
+  thread=Thread(target=control_robot)
+  thread.start()
+  MainApp().run()
+  window_is_open=False
+
 if __name__ == "__main__":
   main()
-  #test()
